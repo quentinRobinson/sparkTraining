df3f93e093fbe69b8bf7d398f962c5850306b9d6
quentinRobinson <quentin.t.robinson@gmail.com>
2021-05-17 10:03:44 -0500

    fixed gitignore

--- ratings-counter.py
+++ ratings-counter.py
@@ -11,3 +11,6 @@ result = ratings.countByValue()
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
+
+
+#temp
